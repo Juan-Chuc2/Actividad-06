0ac5b115de727a4a753dc6746b5e5a9931747efe
Juan Diego <chucjuan508@gmail.com>
2025-07-24 09:50:17 -0800

    1 commit menu

---
+++ Actividad 06.py
@@ -0,0 +1,13 @@
+def saludar():
+    print("Bienvenido al menu de operaciones matematicas")
+while True:
+    print("\n ---MENU---")
+    print("1. Ingresar n numeros y mostrar")
+    print("2. Calcular el area de un triangulo")
+    print("3. Verificar si un numero es par o impar")
+    print("4. Calcular el promedio de n calificaciones")
+    print("5. Ingresar n numeros y verificar cual es el mayor y menor de ellos")
+    print("6. Salir")
+    option = input("Elija una de las opciones (1-6)")
+
+
