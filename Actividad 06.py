52cbcdab246cec7077072be7be228ef2ce5afee1
Juan Diego <chucjuan508@gmail.com>
2025-07-24 17:19:27 -0800

    3 commit funciones de caso  y caso 2 terminado

--- Actividad 06.py
+++ Actividad 06.py
@@ -7,14 +7,14 @@ def calcular_promedio (numbers):
 def contar_positivos_negativos(numbers):
     positivos = 0
     negativos = 0
-
     for number in numbers:
         if number > 0:
             positivos += 1
         elif number < 0:
             negativos += 1
     return positivos, negativos
-
+def calcular_el_area_de_triangulo(base, altura):
+    return base*altura/2
 while True:
     print("\n ---MENU---")
     print("1. Ingresar n numeros y mostrar")
@@ -23,13 +23,13 @@ while True:
     print("4. Calcular el promedio de n calificaciones")
     print("5. Ingresar n numeros y verificar cual es el mayor y menor de ellos")
     print("6. Salir")
-    option = input("Elija una de las opciones (1-6)")
+    option = input("Elija una de las opciones (1-6) ")
     match option:
         case "1":
-            number_user = int(input("Cuantos numeros desea ingresar"))
+            number_user = int(input("Cuantos numeros desea ingresar "))
             numbers=[]
             for i in range(number_user):
-                number = float(input("Ingrese los numneros"))
+                number = float(input("Ingrese los numneros "))
                 numbers.append(number)
             positivos, negativos = contar_positivos_negativos(numbers)
             print("\n --Resultados--")
@@ -37,3 +37,8 @@ while True:
             print(f"El promedio de los numeros es: {calcular_promedio(numbers)}")
             print(f"la cantidad de numeros negativos son: {negativos}")
             print(f"la cantidad de numeros positivos son: {positivos}")
+        case "2":
+            print("\n --Calculo del area de un triangulo--")
+            base = float(input("Ingrese la medida de la base "))
+            altura = float(input("Ingrese la medida de la altura del triangulo "))
+            print(f"El area del triangulo es: {calcular_el_area_de_triangulo(base,altura)}")
